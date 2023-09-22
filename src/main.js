c08217092b0f461685b468b288c6b902b39e44f7
kevenallan <kevenallan98@gmail.com>
2023-09-22 20:29:55 -0300

    Adicionando o Primevue e Primeicon

--- src/main.js
+++ src/main.js
@@ -5,5 +5,11 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import 'primevue/resources/themes/saga-blue/theme.css'; // Escolha o tema desejado
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+
+
 
 createApp(App).use(router).mount('#app')
